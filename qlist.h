a81cc03c34ad80247c574604e358f2f22128c201
WayneLin1992 <62940642+WayneLin1992@users.noreply.github.com>
2021-03-13 11:32:45 +0800

    Add quick sort header and test bench
    Mission: qsort in test bench and record the sort time in txt

---
+++ qlist.h
@@ -0,0 +1,146 @@
+#ifndef LINKEDLIST_QUICKSORT_H_INCLUDED
+#define LINKEDLIST_QUICKSORT_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct __node {
+    int value;
+    struct __node *next;
+} node_t;
+
+static inline void list_add_node_t(node_t **list, node_t *node_t) {
+    node_t->next = *list;
+    *list = node_t;
+}
+
+static inline void list_concat(node_t **left, node_t *right) {
+    while (*left)
+        left = &((*left)->next);
+    *left = right;
+}
+
+void quicksort(node_t **list)
+{
+    if (!*list)
+        return;
+
+    node_t *pivot = *list;
+    int value = pivot->value;
+    node_t *p = pivot->next;
+    pivot->next = NULL;
+
+    node_t *left = NULL, *right = NULL;
+    while (p) {
+        node_t *n = p;
+        p = p->next;
+        list_add_node_t(n->value > value ? &right : &left, n);
+    }
+
+    quicksort(&left);
+    quicksort(&right);
+
+    node_t *result = NULL;
+    list_concat(&result, left);
+    list_concat(&result, pivot);
+    list_concat(&result, right);
+    *list = result;
+}
+
+void quicksort_nonrecursive(node_t **list){
+    #define MAX_AY 100000
+    #define  MAX_LEVELS  1000
+    node_t *ptr = *list;
+    int arr[MAX_AY ] ;
+    int j = 0;
+    while(ptr){
+        arr[j++] = ptr->value;
+        ptr = ptr->next;
+    }
+
+    int element = 20;
+
+    int  piv, beg[MAX_LEVELS], end[MAX_LEVELS], i=0, L, R ;
+    beg[0]=0; end[0]=element;
+    while (i>=0) {
+        L=beg[i]; R=end[i]-1;
+        if (L<R) {
+          piv=arr[L]; if (i==element-1) return ;
+          while (L<R) {
+            while (arr[R]>=piv && L<R)
+                R--;
+            if (L<R)
+                arr[L++]=arr[R];
+            while (arr[L]<=piv && L<R)
+                L++;
+            if (L<R)
+                arr[R--]=arr[L]; }
+          arr[L]=piv;
+          beg[i+1]=L+1;
+          end[i+1]=end[i];
+          end[i++]=L;
+          }
+        else {
+          i--; }
+    }
+    node_t*ptr1 = *list;
+    i = 0;
+    while(ptr1){
+        ptr1->value = arr[i++];
+        ptr1 = ptr1->next;
+    }
+}
+static bool list_is_ordered(node_t *list) {
+    bool first = true;
+    int value;
+    while (list) {
+        if (first) {
+            value = list->value;
+            first = false;
+        } else {
+            if (list->value < value)
+                return false;
+            value = list->value;
+        }
+        list = list->next;
+    }
+    return true;
+}
+
+static void list_display(node_t *list) {
+    printf("%s IN ORDER : ", list_is_ordered(list) ? "   " : "NOT");
+    while (list) {
+        printf("%d ", list->value);
+        list = list->next;
+    }
+    printf("\n");
+}
+
+int random(void){
+    int a = rand();
+    return a;
+}
+
+static void list_free(node_t **list){
+    node_t *ptr = *list;
+    while(*list){
+        ptr = (*list)->next;
+        free(*list);
+        *list = ptr;
+    }
+}
+
+static inline node_t* list_make_node_t(node_t *list, int num){
+    node_t *p = malloc(sizeof(node_t));
+    if(p==NULL){
+        return list;
+    }else{
+        p->value = num;
+        p->next = NULL;
+        list_add_node_t(&list,p);
+    }
+    return list;
+}
+
+#endif // LINKEDLIST_QUICKSORT_H_INCLUDED
---
+++ testbenchq.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdint.h>
+
+#include "qlist.h"
+
+int main()
+{
+    srand(time(NULL));
+    FILE *fp = fopen("sort_timeqn.txt", "w");
+    if (!fp) {
+        perror("failed to open sort_timeqn.txt");
+        exit(EXIT_FAILURE);
+    }
+    fprintf(fp,"count\tlist_sort(nonqsort)\n");
+    for(int i=10000; i<100000;i= i+1000){
+        node_t *list = NULL;
+        int count = i;
+        while (count--){
+            list = list_make_node_t(list, random() % 1024);
+        }
+        struct timespec time_start;
+        struct timespec time_end;
+        long during = 0;
+        clock_gettime(CLOCK_MONOTONIC, &time_start);
+        quicksort(&list);
+        clock_gettime(CLOCK_MONOTONIC, &time_end);
+        during = time_end.tv_nsec - time_start.tv_nsec;
+        list_free(&list);
+        fprintf(fp,"%d\t%ld \n",i,during);
+    }
+    fclose(fp);
+    return 0;
+}
